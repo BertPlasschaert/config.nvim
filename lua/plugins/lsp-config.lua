e22438bcf7f03b89af38416824f5142bc070f486
BertPlasschaert <bert.plasschaert@outlook.com>
2024-08-05 20:26:53 +0000

    wip config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,31 +22,51 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.ruff.setup({})
-      -- lspconfig.pylsp.setup({})
-      local settings = {
-        pylsp = {
-          plugins = {
-            -- type checker
-            pylsp_mypy = { enabled = true },
-            -- auto-completion options
-            jedi_completion = { fuzzy = true },
-            -- formatter options
-            black = { enabled = false},
-            autopep8 = { enabled = false },
-            yapf = { enabled = false },
-            -- linter options
-            pylint = { enabled = false},
-            pyflakes = { enabled = false },
-            pycodestyle = { enabled = false },
-            -- import sorting
-            pyls_isort = { enabled = false},
+      lspconfig.ruff.setup({
+        init_options = {
+          settings = {
+            organizeImports = true,
+            lineLength = 79,
+            lint = {
+              extendSelect = {"ALL"},
+              ignore = {
+                "UP", -- pyupgrade
+                "FURB",
+                "ANN", -- Do not enforce type annotations
+                "C408", -- Prefer collecting call over literal
+                "TD", -- no TODO linting
+                "D100", -- no missing docstring in public module
+                "D101", -- no missing docstring in public class 
+              }
+            },
           },
         },
-      },
-
-
-      -- })
+      })
+      lspconfig.pylsp.setup({
+        settings = {
+          pylsp = {
+            plugins = {
+              -- type checker
+              pylsp_mypy = { enabled = false},
+              -- auto-completion options
+              jedi_completion = { fuzzy = false},
+              -- formatter options
+              black = { enabled = false},
+              autopep8 = { enabled = false },
+              flake8 = { enabled = false },
+              yapf = { enabled = false },
+              -- linter options
+              pylint = { enabled = false},
+              pyflakes = { enabled = false},
+              pycodestyle = { enabled = false},
+              mccabe = { enabled = false},
+              -- import sorting
+              pyls_isort = { enabled = false},
+              rope_autoimport = {enabled = false},
+            },
+          },
+        },
+      })
       vim.keymap.set("n","<leader>qf",vim.lsp.buf.hover, {})
       vim.keymap.set("n","gd",vim.lsp.buf.definition, {})
       vim.keymap.set("n","<leader>ca", vim.lsp.buf.code_action, {})
