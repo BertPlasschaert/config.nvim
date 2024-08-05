b50173f6824a0342aaaa9b6f0a88755a205a6f75
BertPlasschaert <bert.plasschaert@outlook.com>
2024-08-05 17:50:06 +0000

    wip config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,9 +11,9 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "basedpyright",
           "ruff",
-      }
+          "pylsp",
+        }
       })
     end
   },
@@ -22,8 +22,31 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.basedpyright.setup({})
       lspconfig.ruff.setup({})
+      -- lspconfig.pylsp.setup({})
+      local settings = {
+        pylsp = {
+          plugins = {
+            -- type checker
+            pylsp_mypy = { enabled = true },
+            -- auto-completion options
+            jedi_completion = { fuzzy = true },
+            -- formatter options
+            black = { enabled = false},
+            autopep8 = { enabled = false },
+            yapf = { enabled = false },
+            -- linter options
+            pylint = { enabled = false},
+            pyflakes = { enabled = false },
+            pycodestyle = { enabled = false },
+            -- import sorting
+            pyls_isort = { enabled = false},
+          },
+        },
+      },
+
+
+      -- })
       vim.keymap.set("n","<leader>qf",vim.lsp.buf.hover, {})
       vim.keymap.set("n","gd",vim.lsp.buf.definition, {})
       vim.keymap.set("n","<leader>ca", vim.lsp.buf.code_action, {})
