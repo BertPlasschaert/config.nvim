2bb5f5074384423ebf16e756cad01e5a09bfd255
BertPlasschaert <bert.plasschaert@outlook.com>
2024-08-05 20:37:39 +0000

    wip config

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
+import logging
 import math
+import os
+from pathlib import Path
 
-def test_func(input:int):
 
-    print("this")
-    print(input)
+def test_func(finput: int):
+    """Tests the input and prints it.
+
+    :param finput: this is a first param
+    :type finput: int
+
+    :returns: none
+    :rtype: none
+
+    """
+    logging.info("this")
+    logging.info(finput)
+    print("test")
 
 
 test_func("test")
+test_func((6, 6))
+test_func(5.5)
 
+test_func(6)
+
+kkskskskskskskskskskskskskskskskskskskskskkskskskskskskskskskskskskksksjajajajajajja= True
