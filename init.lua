7cc8d24dac0936e7693d0bd763fb227c8e9fe7b1
BertPlasschaert <bert.plasschaert@outlook.com>
2024-08-10 20:20:17 +0000

    wip config

--- init.lua
+++ init.lua
@@ -1,8 +1,44 @@
+vim.g.mapleader = " "
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.g.mapleader = " "
+
+-- Enable mouse mode, can be useful for resizing splits
+vim.opt.mouse = "a"
+
+-- Mode already in lualine plugin
+vim.opt.showmode = false
+
+-- Save undo history by file
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Configure new splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitution (search and replace) live
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+
+require("config.keymaps")
 require("config.lazy")
--- lua/plugins/completions_type.lua
+++ lua/plugins/completions_type.lua
@@ -1,25 +1,23 @@
 local var = {
   {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
     "L3MON4D3/LuaSnip",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
     },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end
   },
   {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
-          expand =  function(args)
+          expand = function(args)
             require("luasnip").lsp_expand(args.body)
           end,
         },
@@ -28,22 +26,21 @@ local var = {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-y>"] = cmp.mapping.confirm({select = true}),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = {
-          {name = "nvim_lsp"},
-          {name = "nvim_lua"},
-          {name = "luasnip"},
-          {name = "buffer"},
-        }
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
+        }),
       })
-    end
-  }
+    end,
+  },
 }
 
-
-return var
+return {}
