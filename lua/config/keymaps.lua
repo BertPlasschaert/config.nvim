a3a25f51950a7a2b44c8b8ec23c2cc3efd70f764
Bert Plasschaert <bert.plasschaert@outlook.com>
2024-08-23 21:37:21 +0200

    wip config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
@@ -11,10 +11,10 @@ keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
-keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- Use CTRL+<hjkl> to switch between windows
+-- See `:help wincmd` for a list of all window commands
 keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
