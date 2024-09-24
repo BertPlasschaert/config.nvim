0a14c2c6564ad331d0d2ff5a454464c8233989fc
Bert Plasschaert <bert.plasschaert@outlook.com>
2024-09-24 08:18:25 +0200

    installed ripgrep

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -1,11 +1,10 @@
-local old = { {
+local active_conf = { {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      -- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     end
@@ -131,4 +130,4 @@ local new = {
 -- vim: ts=2 sts=2 sw=2 et
 
 
-return old
+return active_conf
