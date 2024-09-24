0ca78db3f5c2c0798f2bbc950fa2cf2f41dd4e52
Bert Plasschaert <bert.plasschaert@outlook.com>
2024-09-24 10:47:26 +0200

    added light theme for easy swtich

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,9 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    -- dak theme
     vim.cmd.colorscheme "catppuccin"
+    -- light theme
+    -- vim.cmd.colorscheme "catppuccin-latte"
   end
 }
